f545d8bc1b1d314e1e216c50eb36697fc1a81d12
Andrey Ivanov <aportmail2@gmail.com>
2023-06-04 17:45:34 +0300

    Вычисление факториала

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,20 +1,3 @@
 function sum(m, n) {
   return m + n;
 }
-
-console.log(typeof typeof 5);
-
-
-function factorial(n) {
-  let factorial = n;
-  if (n === 0) {
-    factorial = 1;
-  } else {
-    for (let i = n; i > 1; i--) {
-      n -= 1;
-      factorial *= n;
-    }
-  }
-  return factorial;
-}
-console.log(factorial(3));
