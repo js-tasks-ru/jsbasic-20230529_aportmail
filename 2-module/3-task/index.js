3a6e92c15527d2da20c98c62853c68f102899754
Andrey Ivanov <aportmail2@gmail.com>
2023-06-08 19:50:56 +0300

    Калькулятор не проходит тесты

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,22 @@
 let calculator = {
-  // ваш код
+  read() {
+    this.a = Number(prompt('Введите число а'));
+    this.b = Number(prompt('Введите число b'));
+  },
+
+  sum() {
+    return this.a + this.b;
+  },
+
+  mul() {
+    return this.a * this.b;
+  },
 };
+
+// calculator.read();
+
+// console.log(calculator.sum());
+// console.log(calculator.mul());
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
