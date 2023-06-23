eebb9be8532651f62f44ee5c1386f720f865d6e9
Andrey Ivanov <aportmail2@gmail.com>
2023-06-23 10:50:44 +0300

    Проверка на спам

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,8 @@
 function checkSpam(str) {
-  // ваш код...
+  strLowerCase = str.toLowerCase();
+
+  if (strLowerCase.includes('1xBet'.toLowerCase()) || strLowerCase.includes('XXX'.toLowerCase())) {
+    return true;
+  };
+  return false;
 }
