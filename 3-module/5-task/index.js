fd64c61a3014167929f038a6bd284f7ed696d707
Andrey Ivanov <aportmail2@gmail.com>
2023-07-03 12:32:13 +0300

    Поиск max и min

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,15 @@
+let result = {};
+
 function getMinMax(str) {
-  // ваш код...
+  let splitStr = str.split(' ');
+  let onlyNumber = splitStr.map(item => Number(item)).filter(item => item);
+
+  result.min = Math.min.apply(null, onlyNumber);
+  result.max = Math.max.apply(null, onlyNumber);
+
+  return result;
 }
+
+let inputData = '1 и -5.8 или 10 хотя 34 + -5.3 и 73';
+
+console.log(getMinMax(inputData)); // { min: -5.8, max: 73  }
