ee292395423230ccfdd6ba01f402f4601f0d45bb
Andrey Ivanov <aportmail2@gmail.com>
2023-07-24 11:35:17 +0300

    Текст исчезает и появляется при нажатии на кнопку

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,7 @@
 function toggleText() {
-  // ваш код...
-}
+  let text = document.querySelector('#text');
+  let button = document.querySelector('.toggle-text-button');
+
+  button.addEventListener('click', () => { (!text.hidden) ? text.hidden = true : text.hidden = false }
+)};
+
