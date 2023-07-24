4a6d726922f769e0ccd958f3b4db30bb506c5fa3
Andrey Ivanov <aportmail2@gmail.com>
2023-07-24 11:04:41 +0300

    Кнопка скрывается при нажатии

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,14 +3,13 @@ function highlight(table) {
   for (let i = 1; i < rows.length; i++) {
     let dataAvailableValue = rows[i].cells[3].getAttribute('data-available');
     if (dataAvailableValue === null) {
-      rows[i].setAttribute('hidden', true);
+      rows[i].hidden = true;
     } else {
       (dataAvailableValue === 'true') ? rows[i].classList.add('available') : rows[i].classList.add('unavailable');
     }
 
     let genderValue = rows[i].cells[2].innerHTML;
     (genderValue === 'm') ? rows[i].classList.add('male') : rows[i].classList.add('female')
-    console.log(rows[i].classList);
 
     let ageValue = rows[i].cells[1].innerHTML;
     if (ageValue < 18) {
