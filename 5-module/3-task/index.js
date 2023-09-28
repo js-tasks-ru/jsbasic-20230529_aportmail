fd23dc2b4aedaab6ff5de302717fe43cd6f5bdce
Andrey Ivanov <aportmail2@gmail.com>
2023-09-28 18:47:03 +0300

    промежуточный коммит

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,17 @@
 function initCarousel() {
-  // ваш код...
+  let arrowRight = document.querySelector('.carousel__arrow_right');
+  let carouselInner = document.querySelector('.carousel__inner');
+  let carouselWidth = carouselInner.offsetWidth + 'px';
+
+  arrowRight.addEventListener('click', () => {
+    let carouselShift = 0;
+    function shift() {
+      carouselShift = carouselShift + carouselWidth;
+      carouselInner.style.transform = `translateX(-${carouselShift})`;
+    }
+    shift();
+    console.log(carouselWidth);
+
+  });
+
 }
