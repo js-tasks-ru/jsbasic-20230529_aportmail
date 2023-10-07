6c54cd704fee2a852c6459122c5b3b4d400b6acd
andrey ivanov <aportmail@gmail.com>
2023-10-07 14:33:34 +0300

    Промежуточный коммит

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,33 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class ProductCard {
   constructor(product) {
+    this.product = product;
+    console.log(product);
+    // this.render();
+  }
+
+  render() {
+    this.elem = document.createElement('DIV');
+    this.elem.classList.add('card');
+
+    this.elem.innerHTML = this.product
+      .map(({ name, price, category, image, id }) => 
+        `
+      <div class="card__top">
+        <img src="/assets/images/products/laab_kai_chicken_salad.png" class="card__image" alt="product">
+        <span class="card__price">${price}</span>
+      </div>
+      <div class="card__body">
+        <div class="card__title">${name}</div>
+        <button type="button" class="card__button">
+          <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+        </button>
+      </div>
+    
+    `)
+      .join('');
+
+    return console.log(this.elem);
   }
 }
