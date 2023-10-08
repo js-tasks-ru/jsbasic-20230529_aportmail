239e81fc3a60534bbd1d9c472afd58d645f3ba39
Andrey Ivanov <aportmail2@gmail.com>
2023-10-08 10:27:10 +0300

    Создание карточки товара и кастомного события

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,33 +1,45 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class ProductCard {
+  #elem;
   constructor(product) {
     this.product = product;
     console.log(product);
-    // this.render();
+    this.render();
+  }
+
+  get elem() {
+    return this.#elem
   }
 
   render() {
-    this.elem = document.createElement('DIV');
-    this.elem.classList.add('card');
-
-    this.elem.innerHTML = this.product
-      .map(({ name, price, category, image, id }) => 
-        `
-      <div class="card__top">
-        <img src="/assets/images/products/laab_kai_chicken_salad.png" class="card__image" alt="product">
-        <span class="card__price">${price}</span>
-      </div>
+    this.#elem = createElement(`
+      <div class="card">
+        <div class="card__top">
+          <img src="/assets/images/products/${this.product.image}" class="card__image" alt="product">
+          <span class="card__price">${'€' + this.product.price.toFixed(2)}</span>
+        </div>
       <div class="card__body">
-        <div class="card__title">${name}</div>
-        <button type="button" class="card__button">
+        <div class="card__title">${this.product.name}</div>
+            <button type="button" class="card__button">
           <img src="/assets/images/icons/plus-icon.svg" alt="icon">
-        </button>
+            </button>
+          </div>
       </div>
-    
     `)
-      .join('');
 
-    return console.log(this.elem);
+
+    this.#elem.addEventListener('click', this.productAddEvent);
+
+    return this.#elem;
+  }
+
+  productAddEvent = (event) => {
+    let myEvent = new CustomEvent('product-add', {
+      detail: this.product.id,
+      bubbles: true,
+    });
+
+    this.elem.dispatchEvent(myEvent);
   }
 }
