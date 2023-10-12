64a1a3d5eb6cc9df791d6855e0e4136b08f0d5f4
Andrey Ivanov <aportmail2@gmail.com>
2023-10-12 18:39:44 +0300

    промежуточный коммит

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,5 +2,45 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
+    this.render();
+  }
+
+  render() {
+    this.elem = createElement(`
+      <div class="modal">
+      <!--Прозрачная подложка перекрывающая интерфейс-->
+      <div class="modal__overlay"></div>
+
+      <div class="modal__inner">
+        <div class="modal__header">
+          <!--Кнопка закрытия модального окна-->
+          <button type="button" class="modal__close">
+            <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+          </button>
+
+          <h3 class="modal__title">
+            Вот сюда нужно добавлять заголовок
+          </h3>
+        </div>
+
+        <div class="modal__body">
+          A сюда нужно добавлять содержимое тела модального окна
+        </div>
+      </div>
+
+    </div>
+    `)
+  }
+
+  open() {
+
+  }
+
+  setTitle() {
+
+  }
+
+  setBody() {
+
   }
 }
